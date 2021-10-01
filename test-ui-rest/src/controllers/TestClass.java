17378f016dcf33020a958f781f17d70df049d470
Arina <adamovaarin@yandex.ru>
2021-10-02 00:15:41 +0300

    static field remove

--- test-ui-rest/src/controllers/TestClass.java
+++ test-ui-rest/src/controllers/TestClass.java
@@ -11,25 +11,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.io.IOException;
 import java.io.Reader;
 
 @SpringBootApplication
 public class TestClass {
-    private static PersonSystem personSystem;
 
     public static void main(String[] args) {
-        SqlSessionFactory sqlSessionFactory;
-        MyMapper newMapper;
-        Reader reader;
-        try {
-            reader = Resources
-                    .getResourceAsReader("mybatis-config.xml");
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-            SqlSession s = sqlSessionFactory.openSession();
-            newMapper = s.getMapper(MyMapper.class);
-            Repository r = new Repository(newMapper, s);
-            TestClass.personSystem = new PersonSystem(r);
 
+        try {
             SpringApplication.run(TestClass.class, args);
         } catch (Exception e) {
             e.printStackTrace();
@@ -37,7 +27,16 @@ public class TestClass {
     }
 
     @Bean
-    public PersonSystem personSystem(){
-        return personSystem;
+    public PersonSystem personSystem() throws IOException {
+        SqlSessionFactory sqlSessionFactory;
+        MyMapper newMapper;
+        Reader reader;
+        reader = Resources
+                .getResourceAsReader("mybatis-config.xml");
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        SqlSession s = sqlSessionFactory.openSession();
+        newMapper = s.getMapper(MyMapper.class);
+        Repository r = new Repository(newMapper, s);
+        return new PersonSystem(r);
     }
 }
