29885e42518a6b43da62750c7eb948b88c6f4374
Arina <adamovaarin@yandex.ru>
2021-10-24 19:53:15 +0300

    vue fixes

--- test-beans/src/beans/Person.java
+++ test-beans/src/beans/Person.java
@@ -1,14 +1,14 @@
 package beans;
 
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
 
 @Builder
-@Getter
-@Setter
+@Data
 public class Person {
     private int id;
     private String lastName;
@@ -16,62 +16,5 @@ public class Person {
     private String patronymic;
     private Date dateOfBirth;
     private Gender personGender;
-
-    public Person(int id, String lastName, String firstName, String patronymic, Date dateOfBirth, Gender personGender){
-        this.id = id;
-        this.lastName = lastName;
-        this.firstName = firstName;
-        this.patronymic = patronymic;
-        this.dateOfBirth = dateOfBirth;
-        this.personGender = personGender;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public String getPatronymic() {
-        return patronymic;
-    }
-
-    public Gender getPersonGender() {
-        return personGender;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setPatronymic(String patronymic) {
-        this.patronymic = patronymic;
-    }
-
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public void setPersonGender(Gender personGender) {
-        this.personGender = personGender;
-    }
 
 }
--- test-ui-vue/src/main.js
+++ test-ui-vue/src/main.js
@@ -3,8 +3,6 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.use(PrimeVue);
-Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
